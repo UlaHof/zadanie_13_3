11aaa3f09eb8db960f7353051f41f57787d85b95
Ula <chcetylkoobejrzecfilm@gmail.com>
2018-03-01 21:29:34 +0100

    Added js file

---
+++ program.js
@@ -0,0 +1,20 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function(){
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting App!\n');
+                process.exit();
+                break;
+            case '/version':
+                process.stdout.write(process.version);
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+            }
+        }
+    });
+
